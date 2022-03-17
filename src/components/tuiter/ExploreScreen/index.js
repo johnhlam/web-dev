1c6d6c1508e2eb19299d8cec0a97bd151402df5d
johnlam7707 <johnlam7707@gmail.com>
2022-03-17 15:26:08 -0400

    Hide WhoToFollowList in XS screen

--- src/components/tuiter/ExploreScreen/index.js
+++ src/components/tuiter/ExploreScreen/index.js
@@ -12,7 +12,7 @@ const ExploreScreen = () => {
                  style={{"position": "relative"}}>
                 <ExploreComponent/>
             </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
         </div>
