7842425e69c4d8687efb50ffc7fbe5bd026b493f
johnlam7707 <johnlam7707@gmail.com>
2022-03-17 15:25:34 -0400

    Moved non-react assignment links to labs page

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -11,17 +11,25 @@ const Labs = () => {
   return (
     <>
       <h1>Labs</h1>
-      <ConditionalOutput/>
-      <Styles/>
-      <Classes/>
-      <TodoItem/>
-      <TodoList/>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
+      <TodoItem />
+      <TodoList />
       <Link to="/hello">
         Hello
       </Link> |
       <Link to="/tuiter">
         Tuiter
       </Link>
+      <h1>Non React.js Assignments</h1>
+      <ul>
+        <li><a href="labs/a2/index.html">Assignment 2 Labs</a></li>
+        <li><a href="labs/a3/css/index.html">Assignment 3 Labs</a></li>
+        <li><a href="labs/a4/bootstrap/index.html">Assignment 4 Labs</a></li>
+        <li><a href="labs/a5/js/index.html">Assignment 5 Labs</a></li>
+        <li><a href="tuiter/navigation.html">Tuiter</a></li>
+      </ul>
     </>
   );
 };
